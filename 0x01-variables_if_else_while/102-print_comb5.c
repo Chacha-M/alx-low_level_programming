5cfa6b166f4ce7a767fc07cdba1b84c43d8556ef
Chacha-M <charitymuteti4@gmail.com>
2022-07-01 11:27:29 -0700

    My task 12 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,11 +24,11 @@ op2 = (e * 10) + i;
 if (op1 < op2)
 {
 putchar(h);
-putchar(g);															
+putchar(g);
 putchar(' ');
 putchar(e);
 putchar(i);
-if (h == 57 && g == 56 && e == 57 && i == 57)											
+if (h == 57 && g == 56 && e == 57 && i == 57)
 break;
 putchar(',');
 putchar(' ');
