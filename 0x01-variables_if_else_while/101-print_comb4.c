76d0ceac95fa1c159bb72f2f50958c458c07b147
Chacha-M <107112309+Chacha-M@users.noreply.github.com>
2022-07-01 21:06:13 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,36 +17,24 @@ while (i < 58)
 g = 48;
 while (g < 58)
 {	
-if (e != i && e != g && i != g && e < i && i < g)										{
+if (e != i && e != g && i != g && e < i && i < g)
+{
 putchar(e);
 putchar(i);
 putchar(g);
 if (i == 56 && e == 55 && g == 57)
-																										{
-																																																																							break;
-
-																																																																												}
-
-																																																															putchar(',');
-
-																																																																				putchar(' ');
-
-																																																																								}
-i
-																																				g++;
-
-																																							}
-
-																										i++;
-
-																												}
-
-															e++;
-
-																}
-
-							putchar('\n');
-
-								return (0);
-
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+g++;
+}
+i++;
+}
+e++;
+}
+putchar('\n');
+return (0);
 }
