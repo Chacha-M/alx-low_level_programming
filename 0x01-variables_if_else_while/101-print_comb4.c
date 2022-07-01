bf7d19fb9664435dd03c5e8e15dc552f16707171
Chacha-M <charitymuteti4@gmail.com>
2022-07-01 10:42:51 -0700

    My task 1 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,16 +22,31 @@ putchar(e);
 putchar(i);
 putchar(g);
 if (i == 56 && e == 55 && g == 57)
-{
-break;
-}
-putchar(',');
-putchar(' ');
-}
-g++;
-}
-i++;}
-e++;																}
-putchar('\n');
-return (0);
+																										{
+																																																																							break;
+
+																																																																												}
+
+																																																															putchar(',');
+
+																																																																				putchar(' ');
+
+																																																																								}
+i
+																																				g++;
+
+																																							}
+
+																										i++;
+
+																												}
+
+															e++;
+
+																}
+
+							putchar('\n');
+
+								return (0);
+
 }
