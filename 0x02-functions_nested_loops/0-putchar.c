b9175b2c73203e56b2618960a547ff5e77bdf55e
Chacha-M <107112309+Chacha-M@users.noreply.github.com>
2022-07-05 16:53:43 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,5 @@
 #include <unistd.h>
 #include "main.h"
-
-
-
 /**
  *
  *  * main - Prints _putchar, followed by a new line.
@@ -14,27 +11,14 @@
  *     */
 
 int main(void);
-
 {
-
-		int str[] = {127, 112, 117, 116, 99, 104, 97, 114};
-
-			int count, sz;
-
-
-
-				sz = sizeof(str) / sizeof(int);
-
-					for (count = 0; count < sz; count++)
-
-							{
-
-										_putchar(str[count]);
-
-											}
-
-						_putchar('\n');
-
-							return (0);
-
+int str[] = {127, 112, 117, 116, 99, 104, 97, 114};
+int count, sz;
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz; count++)
+{
+_putchar(str[count]);
+}
+_putchar('\n');
+return (0);
 }
