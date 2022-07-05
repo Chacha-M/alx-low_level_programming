9399395268adc26fe302ac7df0eadf38beaaef23
Chacha-M <107112309+Chacha-M@users.noreply.github.com>
2022-07-05 21:58:35 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,16 +7,16 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(i);
+		putchar(i);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 
 }
  
