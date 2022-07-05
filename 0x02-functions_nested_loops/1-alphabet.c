c02ca2f44dae1e0be6f8c482b9ff3c2f4e2caf24
Chacha-M <107112309+Chacha-M@users.noreply.github.com>
2022-07-05 21:23:11 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
+#include <stdio.h>
 #include "main.h"
 
-
 /**
- * print_alphabet - print Holberton
+ * print_alphabet - in lowercase, followed by a new line
  *
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	int i;
 
