c742a6a3c417c811f7fd4f30143c6049b80c3886
Chacha-M <107112309+Chacha-M@users.noreply.github.com>
2022-07-06 00:19:35 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,9 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
+
 void print_times_table(int n)
+	
 {
 	int i, j, k;
 
@@ -17,7 +19,8 @@ void print_times_table(int n)
 				k = j * i;
 				if (j == 0)
 				{
-					putchar(k + '0');
+					
+			                _putchar(k + '0');
 				} else if (k < 10 && j != 0)
 				{
 					putchar(',');
