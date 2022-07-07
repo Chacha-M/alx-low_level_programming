6bcdc40901446a0882ced5d3dab28d4e10dec896
Chacha-M <107112309+Chacha-M@users.noreply.github.com>
2022-07-07 21:17:15 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * mul - return multiplication of a * b
- * a: first number to multiply
- * b: secondary number to multiply
+ * @a: first number to multiply
+ * @b: secondary number to multiply
  * Return: multiplication of a * b
  */
 
