da44204336961655256702a7b875c70e8766e790
Chacha-M <charitymuteti4@gmail.com>
2022-07-07 10:21:22 -0700

    My task 2 commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,21 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
- * mul - return multiplication of a * b
- * @a: first number to multiply
- * @b: secondary number to multiply
- * Return: multiplication of a * b
- */
+ *
+ *  * mul - return multiplication of a * b
+ *
+ *   * @a: first number to multiply
+ *
+ *    * @b: secondary number to multiply
+ *
+ *     * Return: multiplication of a * b
+ *
+ *      */
+
+
+
 int mul(int a, int b)
 {
-	return (a * b);
+		return (a * b);
 }
