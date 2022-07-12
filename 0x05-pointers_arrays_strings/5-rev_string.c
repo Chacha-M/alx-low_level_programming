7bb7657ac6b342679d230fecd006b386063bb83d
Chacha-M <107112309+Chacha-M@users.noreply.github.com>
2022-07-12 12:23:27 +0300

    My task 5 commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - gives the length of a string
+ * @s: var
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+/**
+ * rev_string-reverse string
+ * @s:var input
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char tmp;
+
+	while (i < j)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++, j--;
+	}
+}
