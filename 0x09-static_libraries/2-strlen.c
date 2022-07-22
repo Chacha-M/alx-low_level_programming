d8f4caf4c7ab23690607d1288257a2229d2b5699
Chacha-M <charitymuteti4@gmail.com>
2022-07-22 15:03:21 -0700

    My library commit

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,18 @@
-int _strlen(char *s) {}
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen-returns the length of a string.
+ * Return:string length
+ * @s:string input
+ */
+
+int _strlen(char *s)
+{
+	const char *mystr;
+	int a;
+
+	mystr = s;
+	a = strlen(mystr);
+	return (a);
+}
--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,1 +1,19 @@
-int _islower(int c) {}
+#include <stdio.h>
+#include"main.h"
+/**
+ * _islower-checks for lowercase
+ * Return: Always 0 is success
+ * @c:charcter input
+ */
+int _islower(int c)
+{
+	int letter;
+
+	letter = c;
+
+	if (letter >= 'a' && letter <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
