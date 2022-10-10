9130ad13639dbfc7807d5dccc22e9b47c92c5010
Charity Muteti <107112309+Chacha-M@users.noreply.github.com>
2022-10-10 12:33:53 +0300

    Create 4-isalpha.c

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isalpha - checks for lowercase or Uppercase char
+*@c: displays character
+* Return:1 if c is lowercase 0 otherwise
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	_putchar('\n');
+}
