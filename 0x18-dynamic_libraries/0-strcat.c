ead4afff9c379028ab0e970f53a80fff37e8bdf9
Charity Muteti <107112309+Chacha-M@users.noreply.github.com>
2022-10-10 10:58:31 +0300

    Create 0-strcat.c

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcat - concatenate two strings
+*@dest: string parameter
+*@src: string parameter
+*Return: char *
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int len, _len, i;
+
+	i = 0;
+	len = strlen(src);
+	_len = strlen(dest);
+
+	for (i = 0; i < len; i++)
+	{
+	dest[_len + i] = src[i];
+	}
+	return (dest);
+}
